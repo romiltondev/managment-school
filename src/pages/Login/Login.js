b888031c1cb356ef176ac55f2bfa7ee349c3838d
romiltondev <156550707+romiltondev@users.noreply.github.com>
2024-03-01 21:56:04 -0300

    created login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import School from '../../school.svg';
+import './style.css'
 export function Login(){
     return(
         <div className="container">
@@ -6,11 +7,19 @@ export function Login(){
                 <div className="heading">
                     <img src={School} alt="logo"/>
                     <h3>Bem vindo</h3>
-                    <p>Entre com a sua conta</p>
+                    <p className="paragraph">Entre com a sua conta</p>
                 </div>
                 <form action="" method="" className="send-form">
-                    <input type="email" placeholder="abc@email.com"/>
-                    <input type="password" placeholder="Sua senha"/>
+                    <div className="input-container">
+                    <i class="fa-regular fa-envelope fa-lg"></i>
+                    <input type="email" placeholder="abc@email.com" className="input-mail"/>
+                    </div>
+                    <div className="input-container">
+                    <i class="fa-solid fa-lock fa-lg"></i>
+                    <input type="password" placeholder="Sua senha" minlength="8" required className="input-password"/>
+                    <i class="fa-solid fa-eye-slash fa-lg "></i>
+                    </div>
+                    
                     <button>Entrar</button>
                 </form>
             </div>
